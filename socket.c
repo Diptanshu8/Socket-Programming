04a39081d1b3eef520975cd7af216b74800c7479
Diptanshu8 <diptanshuj@gmail.com>
2016-06-07 18:05:08 +0530

    THis file contains description of a socket

--- socket.c
+++ socket.c
@@ -1,9 +1,32 @@
-#include <sys/types.h>
-#include <sys/sockets.h>
-#include <netdb.h>
 
-int getaddrinfo(const char *node,        // e.g. "www.example.com" or IP
-                const char *service,     // e.g. "http" or port number
-                const struct addrinfo *hints,
-                struct addrinfo **res
-        );
+/*
+ * socket.c -- socket description in C
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+    struct addrinfo hints, *res, *p;
+    int status,count;
+    char ipstr[INET6_ADDRSTRLEN];
+    if (argc != 2) {
+        fprintf(stderr,"usage: showip hostname\n");
+        return 1;
+                    }
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // AF_INET or AF_INET6 to force version
+    hints.ai_socktype = SOCK_STREAM;
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+            return 2;
+        }
+
+    //socket description
+s = socket(res->ai.family,res->ai.socktype,res->ai.protocol);
